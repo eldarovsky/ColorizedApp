e1135b08eccd3cec45a19e3e3ac487c036b992d1
Eldar Abdullin <60284515+eldarovsky@users.noreply.github.com>
2023-05-25 10:44:19 +0300

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Эльдар Абдуллин on 25.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
